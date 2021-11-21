8d2eac21f322b1bc84fbb5191fc106a5ce8fb1a5
Oleksii Prystavko <Oleksii.Prystavko@infopulse.com>
2021-11-21 02:53:46 +0200

    Add test

---
+++ locustHttpUserTest.py
@@ -0,0 +1,66 @@
+import logging, uuid, random
+from locust import HttpUser, task, constant_throughput
+
+
+class QuickstartUser(HttpUser):
+    wait_time = constant_throughput(10)
+    host = "http://172.27.176.7"
+
+    @task(5)
+    def get_stats(self):
+        self.client.get("/api/getstat", name='Get Stats', verify=False)
+
+    @task(5)
+    def open_list_of_test_cases(self):
+        self.client.get("/api/tests", name='Open list of Test Cases', verify=False)
+
+    @task(1)
+    def create_new_test_case(self):
+        name = uuid.uuid4()
+        description = uuid.uuid4()
+        self.client.post("/api/tests/new", json={'name': f'test nr {name}', 'description': f'test nr {description}'},
+                         name='Create new Test Case', verify=False)
+
+    @task(1)
+    def open_test_case_details(self):
+        test_id = random.randint(1, 10)
+        self.client.get(f'/api/tests/{test_id}', name='Open Test Case details', verify=False)
+
+    @task(3)
+    def change_test_case_status_to_pass(self):
+        test_id = random.randint(1, 10)
+        self.client.post(f'/api/tests/{test_id}/status', json={"status": "PASS"},
+                         name='Change Test Case status to PASS', verify=False)
+
+    @task(1)
+    def update_test_case(self):
+        test_id = random.randint(1, 10)
+        name = uuid.uuid4()
+        description = uuid.uuid4()
+        self.client.put(f'/api/tests/{test_id}', json={'name': f'test nr {name}', 'description': f'test nr {description}'},
+                         name='Update Test Case', verify=False)
+
+    @task(1)
+    def partial_update_test_case(self):
+        test_id = random.randint(1, 10)
+        name = uuid.uuid4()
+        self.client.patch(f'/api/tests/{test_id}', json={'name': f'test nr {name}'},
+                         name='Partial update Test Case', verify=False)
+
+    @task(3)
+    def change_test_case_status_to_fail(self):
+        test_id = random.randint(1, 10)
+        self.client.post(f'/api/tests/{test_id}/status', json={"status": "FAIL"},
+                         name='Change Test Case status to FAIL', verify=False)
+
+    def on_stop(self):
+        self.client.post("/api/auth/logout", name='Logout', verify=False)
+
+    def on_start(self):
+        rs = self.client.post("/api/auth/login", json={"username": "alice", "password": "Qamania123"}, name='Login', verify=False)
+        token = self.client.cookies.get('csrftoken')
+        self.client.headers.update({'X-CSRFToken': token})
+        print(rs.request.body)
+        logging.info(token)
+
+# locust -f --config=master.conf D:\LocustTestMe\locustHttpUserTest.py
